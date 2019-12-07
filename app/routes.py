1b68ddb487696a3544baa5c3b40e1f5b447c33db
honooon <h@onou.win>
2019-12-08 00:56:42 +0800

    add page style

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    soul = '蠢也没那么可怕，毕竟水母没有脑子，也活了6亿年。'
+    return render_template('index.html', soul=soul)
